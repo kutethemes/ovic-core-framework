b25f8dd3f0175eddf2c694209dc9e2624343a3bb
kutethemes <kutethemes@gmail.com>
2019-10-10 10:39:21 +0700

    1

---
+++ config/ovic-core.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+	'scripts' => [
+		/* Chosen v1.8.7 | (c) 2011-2018 by Harvest | MIT License, https://github.com/harvesthq/chosen/blob/master/LICENSE.md */
+		'chosen'         => asset( 'libs/chosen/chosen.min.js' ),
+		/* The Final Countdown for jQuery v2.2.0 (http://hilios.github.io/jQuery.countdown/) */
+		'countdown'      => asset( 'libs/countdown/countdown.min.js' ),
+		/* DataTables v1.10.18 (https://datatables.net/download/) */
+		'datatables'     => asset( 'libs/datatables/datatables.min.js' ),
+		/* Scrollbar v0.2.10 (https://github.com/gromo/jquery.scrollbar/) */
+		'scrollbar'      => asset( 'libs/scrollbar/scrollbar.min.js' ),
+		/* Slick v1.0.2 (http://kenwheeler.github.io) */
+		'slick'          => asset( 'libs/scrollbar/slick.min.js' ),
+		/* Magnific Popup - v1.1.0 (http://dimsemenov.com/plugins/magnific-popup/) */
+		'magnific-popup' => asset( 'libs/magnific-popup/magnific-popup.min.js' ),
+		/* jQuery UI - v1.12.1 (http://jqueryui.com/) */
+		'jquery-ui'      => asset( 'libs/jquery-ui/jquery-ui.min.js' ),
+		/* jQuery UI - v3.4.1 (http://jquery.org/) */
+		'jquery'         => asset( 'libs/jquery/jquery-3.4.1.min.js' ),
+		/* jQuery & Zepto Lazy v1.7.10 - (http://jquery.eisbehr.de/lazy) */
+		'lazyload'       => asset( 'libs/lazyload/lazyload.min.js' ),
+		/* Growl v1.3.5 - (http://ksylvest.github.io/jquery-growl/) */
+		'growl'          => asset( 'libs/growl/growl.min.js' ),
+	],
+	'styles'  => [
+		/* Chosen v1.8.7 | (c) 2011-2018 by Harvest | MIT License, https://github.com/harvesthq/chosen/blob/master/LICENSE.md */
+		'chosen'          => asset( 'libs/chosen/chosen.min.css' ),
+		/* DataTables v1.10.18 (https://datatables.net/download/) */
+		'datatables'      => asset( 'libs/datatables/datatables.min.css' ),
+		/* Font Awesome Free 5.0.8 by @fontawesome - https://fontawesome.com */
+		'font-awesome'    => asset( 'libs/font-awesome/css/fontawesome-all.min.css' ),
+		/* Scrollbar v0.2.10 (https://github.com/gromo/jquery.scrollbar/) */
+		'scrollbar'       => asset( 'libs/scrollbar/scrollbar.min.css' ),
+		/* Slick v1.0.2 (http://kenwheeler.github.io) */
+		'slick'           => asset( 'libs/scrollbar/slick.min.css' ),
+		/* Magnific Popup - v1.1.0 (http://dimsemenov.com/plugins/magnific-popup/) */
+		'magnific-popup'  => asset( 'libs/magnific-popup/magnific-popup.min.css' ),
+		'magnific-effect' => asset( 'libs/magnific-popup/magnific-effect.css' ),
+		/* jQuery UI - v1.12.1 (http://jqueryui.com/) */
+		'jquery-ui'       => asset( 'libs/jquery-ui/jquery-ui.min.css' ),
+		/* Growl v1.3.5 - (http://ksylvest.github.io/jquery-growl/) */
+		'growl'           => asset( 'libs/growl/growl.min.css' ),
+	],
+];
