030e59b99b815582e496cd2636a1cf1605cead64
kutethemes <kutethemes@gmail.com>
2019-10-14 13:19:23 +0700

    1

--- resources/views/Backend/left-sidebar.blade.php
+++ resources/views/Backend/left-sidebar.blade.php
@@ -23,14 +23,15 @@
                 </div>
             </li>
             <li class="active">
-                <a href="index.html"><i class="fa fa-th-large"></i> <span class="nav-label">Dashboards</span> <span
-                            class="fa arrow"></span></a>
+                <a href="#">
+                    <i class="fa fa-users"></i>
+                    <span class="nav-label">Users</span>
+                    <span class="fa arrow"></span>
+                </a>
                 <ul class="nav nav-second-level collapse">
-                    <li><a href="index.html">Dashboard v.1</a></li>
-                    <li><a href="dashboard_2.html">Dashboard v.2</a></li>
-                    <li><a href="dashboard_3.html">Dashboard v.3</a></li>
-                    <li><a href="dashboard_4_1.html">Dashboard v.4</a></li>
-                    <li><a href="dashboard_5.html">Dashboard v.5 </a></li>
+                    <li><a href="#">All Users</a></li>
+                    <li><a href="#">Add New</a></li>
+                    <li><a href="#">Your Profile</a></li>
                 </ul>
             </li>
         </ul>
